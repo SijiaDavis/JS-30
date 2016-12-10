f07b2f8b52ac59f144f022fa7a55e454d7d5eb83
SijiaDavis <arwen@ksu.edu>
2016-12-09 23:25:55 -0500

    Day1 DrumKit finish adding sound effects

--- Day1_DrumKit/app.js
+++ Day1_DrumKit/app.js
@@ -9,22 +9,31 @@ var keyK = document.querySelector(".key-K");
 var keyL = document.querySelector(".key-L");
 
 var mapping = {
-  97: keyA,
-  115: keyS,
-  100: keyD,
-  102: keyF,
-  103: keyG,
-  104: keyH,
-  106: keyJ,
-  107: keyK,
-  108: keyL
+  97: [keyA, 'asset/sounds/bubbles.mp3'],
+  115: [keyS, 'asset/sounds/clay.mp3'],
+  100: [keyD, 'asset/sounds/corona.mp3'],
+  102: [keyF, 'asset/sounds/glimmer.mp3'],
+  103: [keyG, 'asset/sounds/moon.mp3'],
+  104: [keyH, 'asset/sounds/pinwheel.mp3'],
+  106: [keyJ, 'asset/sounds/prism-2.mp3'],
+  107: [keyK, 'asset/sounds/squiggle.mp3'],
+  108: [keyL, 'asset/sounds/strike.mp3']
 }
+
 document.addEventListener('keypress', function(evt){
   console.log(evt.which);
   if (mapping[evt.which]){
-    mapping[evt.which].classList.add("highlight");
+    mapping[evt.which][0].classList.add("highlight");
+    
+    // play sound effect
+    var sound = new Howl({
+      src: [mapping[evt.which][1]]
+    });
+
+    sound.play();
+    
     setTimeout(function(){
-      mapping[evt.which].classList.remove("highlight");
+      mapping[evt.which][0].classList.remove("highlight");
     }, 200);
   }
-})
+});
