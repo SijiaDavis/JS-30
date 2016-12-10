7bf81cfc38734e6458ccb8ed96991f1876d51134
SijiaDavis <arwen@ksu.edu>
2016-12-10 08:38:04 -0500

    modifications

--- Day1_DrumKit/app.js
+++ Day1_DrumKit/app.js
@@ -1,39 +1,36 @@
-var keyA = document.querySelector(".key-A");
-var keyS = document.querySelector(".key-S");
-var keyD = document.querySelector(".key-D");
-var keyF = document.querySelector(".key-F");
-var keyG = document.querySelector(".key-G");
-var keyH = document.querySelector(".key-H");
-var keyJ = document.querySelector(".key-J");
-var keyK = document.querySelector(".key-K");
-var keyL = document.querySelector(".key-L");
-
-var mapping = {
-  97: [keyA, 'asset/sounds/bubbles.mp3'],
-  115: [keyS, 'asset/sounds/clay.mp3'],
-  100: [keyD, 'asset/sounds/corona.mp3'],
-  102: [keyF, 'asset/sounds/glimmer.mp3'],
-  103: [keyG, 'asset/sounds/moon.mp3'],
-  104: [keyH, 'asset/sounds/pinwheel.mp3'],
-  106: [keyJ, 'asset/sounds/prism-2.mp3'],
-  107: [keyK, 'asset/sounds/squiggle.mp3'],
-  108: [keyL, 'asset/sounds/strike.mp3']
+const audioMappings = {
+    97: 'asset/sounds/bubbles.mp3',
+    115: 'asset/sounds/clay.mp3',
+    100: 'asset/sounds/corona.mp3',
+    102: 'asset/sounds/glimmer.mp3',
+    103: 'asset/sounds/moon.mp3',
+    104: 'asset/sounds/pinwheel.mp3',
+    106: 'asset/sounds/prism-2.mp3',
+    107: 'asset/sounds/squiggle.mp3',
+    108: 'asset/sounds/strike.mp3'
 }
 
-document.addEventListener('keypress', function(evt){
-  console.log(evt.which);
-  if (mapping[evt.which]){
-    mapping[evt.which][0].classList.add("highlight");
-    
+document.addEventListener('keypress', playSoundAndHighlight);
+
+function playSoundAndHighlight(evt) {
+    const selector = `#key${evt.which}`;
+    const keyPressed = document.querySelector(selector);
+
+    if (!keyPressed) return;
+
+    keyPressed.classList.add("highlight");
+
     // play sound effect
-    var sound = new Howl({
-      src: [mapping[evt.which][1]]
+    let sound = new Howl({
+        src: [audioMappings[evt.which]]
     });
 
     sound.play();
-    
-    setTimeout(function(){
-      mapping[evt.which][0].classList.remove("highlight");
-    }, 200);
-  }
-});
+}
+
+const keys = document.querySelectorAll(".box");
+for (let i = 0; i < keys.length; i++) {
+    keys[i].addEventListener("transitionend", () => {
+        keys[i].classList.remove("highlight");
+    })
+}
