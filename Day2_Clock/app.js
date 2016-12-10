6165d9b779fa1fc306b0e377f1db59415cf1ebf0
SijiaDavis <arwen@ksu.edu>
2016-12-10 10:45:12 -0500

    Finish day2 clock

---
+++ Day2_Clock/app.js
@@ -0,0 +1,53 @@
+let time, hr, min, sec;
+
+const hrHand = document.querySelector(".hour");
+const minHand = document.querySelector(".minute");
+const secHand = document.querySelector(".second");
+
+// initial set clock face
+setHourHand();
+setMinHand();
+setSecHand();
+
+function setHourHand(){
+  getCurrentTime();
+  const hrDeg = (hr*365/12).toFixed(2);
+  hrHand.style.transform = `rotate(${hrDeg}deg)`;
+}
+
+function setMinHand(){
+  getCurrentTime();
+  const minDeg = (min*365/60).toFixed(2);
+  minHand.style.transform = `rotate(${minDeg}deg)`;
+}
+
+function setSecHand(){
+  getCurrentTime();
+  const secDeg = (sec*365/60).toFixed(2);
+  secHand.style.transform = `rotate(${secDeg}deg)`;
+}
+
+function getCurrentTime(){
+  time = new Date();
+  hr = time.getHours()%12;
+  min = time.getMinutes();
+  sec = time.getSeconds();
+}
+
+// update sec hand every second
+setInterval(function(){
+  setSecHand();
+}, 1000);
+
+// update minute hand every 30 second
+setInterval(function(){
+  setMinHand();
+}, 30000);
+
+// update hour hand every minute
+setInterval(function(){
+  setHourHand();
+}, 60000);
+
+
+
